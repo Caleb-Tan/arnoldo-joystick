7b5672d560a584079287d74eb2ee92e533e2ad02
Caleb-Tan <calebtan23@gmail.com>
2019-06-19 19:14:32 -0700

    fixed self variable

--- gui.py
+++ gui.py
@@ -36,5 +36,5 @@ class Gui:
         y = 0 if y < 0 else y
         x = (float(x)/1023)*self.js_box_width
         y = (float(y)/1023)*self.window_height
-        js_oval_id = draw.oval(x-js, y-js, x+js, y+js)
+        js_oval_id = draw.oval(x-self.js, y-self.js, x+self.js, y+self.js)
 
