783581abb49629ff09403235ef2485e433b7443c
Caleb-Tan <calebtan23@gmail.com>
2019-07-08 21:08:23 -0700

    change threshold for pressure sensor

--- controller.py
+++ controller.py
@@ -16,7 +16,7 @@ mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 class Controller:
     def __init__(self):
         self.deadzone_dist = 125**2
-        self.pressure_threshold = 650
+        self.pressure_threshold = 570
         self.max_x_dist = 75
         self.max_y_dist = 75
         self.init_x = mcp.read_adc(1)
