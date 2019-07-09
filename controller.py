98d0b924ec140dc72d422a651f2ffb9288f72064
Caleb-Tan <calebtan23@gmail.com>
2019-07-08 18:58:35 -0700

    testing pressure average

--- controller.py
+++ controller.py
@@ -21,6 +21,7 @@ class Controller:
         self.max_y_dist = 75
         self.init_x = mcp.read_adc(1)
         self.init_y = mcp.read_adc(0)
+        self.pvals = []
         self.tts = Tts()
         self.in_zone = True
         self.is_pressed = False
@@ -31,10 +32,18 @@ class Controller:
     def get_sentence(self):
         return self.tts.get_sentence()
 
+    def check_pressed(n):
+        if len(self.pvals) > 4:
+            self.pvals.pop(0)
+        self.pvals.append(n)
+        avg_pressure = float(sum(self.pvals)/len(self.pvals))
+        print avg_pressure
+
     def check_vals(self, x, y, n):
         x = x - self.init_x
         y = y - self.init_y
         center_dist = (x**2) + (y**2)
+        self.check_pressed(n)
         if center_dist > self.deadzone_dist and self.in_zone:
             if (-self.max_x_dist < x < self.max_x_dist):
                 self.tts.handle_action(0) if y < 0 else self.tts.handle_action(2)
